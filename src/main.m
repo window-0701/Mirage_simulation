0b9b0e41948fba4bb85c12d4fc50a4eedfc356b3
window-0701 <71367433+window-0701@users.noreply.github.com>
2021-05-12 20:27:40 +0800

    Update main.m

--- src/main.m
+++ src/main.m
@@ -8,25 +8,24 @@ subplot(131);imshow(im1);
 full_image = [];
 
 global n;
-n = 150;   %对图片进行竖直方向n等分
+n = 150;   %Divide the picture equally
 wi = ceil(w/n);
 kMax = 0;
 kMin = 990;
 %compute the trace
 x0 = 0;
-xf = 1.0;    %物距
-y0 = 0.06;     %观察高度
-e = -3.40;  %下现蜃景
-%e = 0.00;  %上现蜃景
+xf = 1.0;    %object distance
+y0 = 0.06;   %height of eye
+e = -3.40;   %inferior mirage
+%e = 0.05;   %superior mirage
 
 for i0 = 0:0.01:pi*3/4
     y10 = 1/tan(i0);
-    %线性模型
+    %linear model
     df = @(x,y)[y(2);(e/sin(i0))^2*y(1)-e/(sin(i0))^2];
     [x,y] = ode45(df,[x0,xf],[y0,y10]);
-    %y(end,1)为最后一个y
     k = self_classify(y(end,1),w);
-    %判断成像位置
+    %Determine imaging position
     if k >= kMax
         kMax = k;
     end
@@ -37,7 +36,7 @@ for i0 = 0:0.01:pi*3/4
         partial_image = im1r(1:wi*k,:,:);
         full_image = [full_image;partial_image];
         subplot(132);
-        plot(x,y(:,1),'-','linewidth',2) %绘图
+        plot(x,y(:,1),'-','linewidth',2) %plot light trajectory
         hold on;
     elseif wi*k > w 
         continue;
@@ -47,7 +46,7 @@ for i0 = 0:0.01:pi*3/4
         partial_image = im1r(wi*(k-1):wi*k,:,:);
         full_image = [full_image;partial_image];
         subplot(132);
-        plot(x,y(:,1),'-','linewidth',2) %绘图
+        plot(x,y(:,1),'-','linewidth',2) %plot light trajectory
         hold on;
     end
 end
@@ -59,5 +58,5 @@ ylim([0 ,14.5]);
 grid on;
 
 subplot(133);
-imshow(full_image);
+imshow(full_image);%show the mirage
 size(full_image)
